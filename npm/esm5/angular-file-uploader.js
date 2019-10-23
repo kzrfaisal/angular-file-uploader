ebbcb0b1f30b801bc6a95a8b4bacf7741a0f5544
anderstornkvist <dev@anderstornkvist.se>
2019-10-23 20:05:57 +0200

    fix: remove npm dir

--- npm/esm5/angular-file-uploader.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { AngularFileUploaderService, AngularFileUploaderComponent, AngularFileUploaderModule } from './public_api';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5ndWxhci1maWxlLXVwbG9hZGVyLmpzIiwic291cmNlUm9vdCI6Im5nOi8vYW5ndWxhci1maWxlLXVwbG9hZGVyLyIsInNvdXJjZXMiOlsiYW5ndWxhci1maWxlLXVwbG9hZGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFJQSxvR0FBYyxjQUFjLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEdlbmVyYXRlZCBidW5kbGUgaW5kZXguIERvIG5vdCBlZGl0LlxuICovXG5cbmV4cG9ydCAqIGZyb20gJy4vcHVibGljX2FwaSc7XG4iXX0=
--- npm/public_api.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/angular-file-uploader.service';
-export * from './lib/angular-file-uploader.component';
-export * from './lib/angular-file-uploader.module';
